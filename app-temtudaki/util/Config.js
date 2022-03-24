13a7a8b96e9caf771b0cc7c5ac8d2800ecbf6e83
Angélica França <gefranca94@gmail.com>
2022-03-24 17:06:12 -0300

    Colocado na config o ip do note samsung

--- app-temtudaki/util/Config.js
+++ app-temtudaki/util/Config.js
@@ -1,5 +1,19 @@
+//PC DE CASA
+// const Config = {
+//     API_URL: "http://192.168.15.34:3000/",
+//     TIMEOUT_REQUEST: 5000,
+//     HEADER_REQUEST: {
+//         Accept: 'application/json',
+        
+//     }
+// }
+// export default Config;
+
+
+
+//PC DA AVANADE
 const Config = {
-    API_URL: "http://192.168.15.7:3000/",
+    API_URL: "http://192.168.15.45:3000/",
     TIMEOUT_REQUEST: 5000,
     HEADER_REQUEST: {
         Accept: 'application/json',
