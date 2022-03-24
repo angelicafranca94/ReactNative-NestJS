7e70afee25a0d5fd2e61e7c389f4c708b2924562
Angélica <angel@DESKTOP-LJEPODJ>
2022-03-23 23:30:46 -0300

    Funcionando no brownser mas ainda dando erro no mobile

---
+++ app-temtudaki/util/Config.js
@@ -0,0 +1,9 @@
+const Config = {
+    API_URL: "http://192.168.15.7:3000/",
+    TIMEOUT_REQUEST: 5000,
+    HEADER_REQUEST: {
+        Accept: 'application/json',
+        
+    }
+}
+export default Config;
